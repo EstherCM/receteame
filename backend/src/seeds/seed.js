20aa798a9325ce98675ca2fbc7aec452e5673504
CrisArranz <c.arranz.arevalo@gmail.com>
2023-01-30 18:38:07 +0100

    Backend: adding seeds script

---
+++ backend/src/seeds/seed.js
@@ -0,0 +1,9 @@
+require('../config/db.config');
+const { Recipe } = require('../model/index');
+const { recipes } = require('./recipes');
+
+Recipe
+  .create(recipes)
+  .then(() => console.info("Created"))
+  .catch(err => console.error(err));
+
