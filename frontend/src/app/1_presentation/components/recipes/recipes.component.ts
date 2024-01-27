3150f884cc7fa9b140194434684c2afd6cc96a34
EstherCM <esther.carreton8@gmail.com>
2024-01-27 19:30:14 +0100

    feat: first commit for frontend

---
+++ frontend/src/app/1_presentation/components/recipes/recipes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes',
+  standalone: true,
+  templateUrl: './recipes.component.html'
+})
+export class RecipesComponent {
+
+}
---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RecipesComponent} from './1_presentation/components/recipes/recipes.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RecipesComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['../styles/header.scss']
+})
+export class AppComponent {
+  title = 'receteame';
+}
