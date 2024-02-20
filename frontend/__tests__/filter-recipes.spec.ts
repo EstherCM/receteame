e34c2e4d6a5456da1f5b64d77516a122b6119a08
EstherCM <esther.carreton8@gmail.com>
2024-02-20 19:12:52 +0100

    test: integration with playwright

--- frontend/__tests__/filter-recipes.spec.ts
+++ frontend/__tests__/filter-recipes.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { RecipeFiltersPage } from './pageObjects/recipe-filters-page';
-import exp from 'constants';
+import { TypeRecipe } from '../src/app/2_domain/models/type-recipe.enum';
 
 test('se visualiza el filtro Nombre', async ({ page }) => {
   const recipeFilterPage = new RecipeFiltersPage(page);
@@ -9,7 +9,7 @@ test('se visualiza el filtro Nombre', async ({ page }) => {
   await recipeFilterPage.navigate();
   await page.waitForSelector(nameLabel);
 
-  const labelExists = await page.$(nameLabel) !== null;
+  const labelExists = (await page.$(nameLabel)) !== null;
   expect(labelExists).toBe(true);
 });
 
@@ -20,7 +20,7 @@ test('se visualiza el filtro Ingredientes', async ({ page }) => {
   await recipeFilterPage.navigate();
   await page.waitForSelector(ingredientsLabel);
 
-  const labelExists = await page.$(ingredientsLabel) !== null;
+  const labelExists = (await page.$(ingredientsLabel)) !== null;
   expect(labelExists).toBe(true);
 });
 
@@ -31,7 +31,7 @@ test('se visualiza el filtro Personas', async ({ page }) => {
   await recipeFilterPage.navigate();
   await page.waitForSelector(peopleLabel);
 
-  const labelExists = await page.$(peopleLabel) !== null;
+  const labelExists = (await page.$(peopleLabel)) !== null;
   expect(labelExists).toBe(true);
 });
 
@@ -42,7 +42,7 @@ test('se visualiza el filtro Tiempo', async ({ page }) => {
   await recipeFilterPage.navigate();
   await page.waitForSelector(timeLabel);
 
-  const labelExists = await page.$(timeLabel) !== null;
+  const labelExists = (await page.$(timeLabel)) !== null;
   expect(labelExists).toBe(true);
 });
 
@@ -53,7 +53,7 @@ test('se visualiza el filtro Tipo', async ({ page }) => {
   await recipeFilterPage.navigate();
   await page.waitForSelector(typeLabel);
 
-  const labelExists = await page.$(typeLabel) !== null;
+  const labelExists = (await page.$(typeLabel)) !== null;
   expect(labelExists).toBe(true);
 });
 
@@ -63,6 +63,46 @@ test('se puede filtrar por Nombre', async ({ page }) => {
   await recipeFilterPage.navigate();
 
   const recipesCount = await recipeFilterPage.filterByName('Mug cake');
-console.log('recipes', recipesCount)
-  expect(recipesCount).toBeGreaterThan(0);
+
+  expect(recipesCount).toBe(1);
+});
+
+test('se puede filtrar por Ingredients', async ({ page }) => {
+  const recipeFilterPage = new RecipeFiltersPage(page);
+
+  await recipeFilterPage.navigate();
+
+  const recipesCount = await recipeFilterPage.filterByIngredients('Huevos');
+
+  expect(recipesCount).toBe(1);
+});
+
+test('se puede filtrar por People', async ({ page }) => {
+  const recipeFilterPage = new RecipeFiltersPage(page);
+
+  await recipeFilterPage.navigate();
+
+  const recipesCount = await recipeFilterPage.filterByPeople(8);
+
+  expect(recipesCount).toBe(0);
+});
+
+test('se puede filtrar por Time', async ({ page }) => {
+  const recipeFilterPage = new RecipeFiltersPage(page);
+
+  await recipeFilterPage.navigate();
+
+  const recipesCount = await recipeFilterPage.filterByTime(20, 40);
+
+  expect(recipesCount).toBe(1);
+});
+
+test('se puede filtrar por Type', async ({ page }) => {
+  const recipeFilterPage = new RecipeFiltersPage(page);
+
+  await recipeFilterPage.navigate();
+
+  const recipesCount = await recipeFilterPage.filterByType(TypeRecipe.snack);
+
+  expect(recipesCount).toBe(1);
 });
--- frontend/__tests__/pageObjects/recipe-filters-page.ts
+++ frontend/__tests__/pageObjects/recipe-filters-page.ts
@@ -2,6 +2,7 @@ import { Page } from 'playwright';
 
 export class RecipeFiltersPage {
   private page: Page;
+  private buttonSelector = '[data-test="searchButton"]';
 
   constructor(page: Page) {
     this.page = page;
@@ -11,13 +12,67 @@ export class RecipeFiltersPage {
     await this.page.goto('http://localhost:4200/');
   }
 
-  async filterByName(value: string) {
-    await this.page.fill('[data-test="recipeNameLabel"]', value);
+  async filterByName(attributeName: string) {
+    await this.page.fill('[data-test="recipeNameLabel"]', attributeName);
+    await this.page.click(this.buttonSelector);
 
-    await this.page.click('[data-test="searchButton"]');
+    return await this.page.evaluate((prefix) => document.querySelectorAll(`[data-test^="${prefix}"]`).length, 'recipe-');
+  }
 
-    return await this.page.evaluate(() => {
-      return document.querySelectorAll('[data-test="Mug Cake"]').length;
-    });
+  async filterByIngredients(value: string) {
+    const selector = '[data-test="recipeIngredientsInput"] input';
+
+    await this.page.waitForSelector(selector);
+    await this.page.fill(selector, value);
+    await this.page.click(this.buttonSelector);
+
+    return await this.page.evaluate((prefix) => document.querySelectorAll(`[data-test^="${prefix}"]`).length, 'recipe-');
+  }
+
+  async filterByPeople(quantity: number) {
+    const selector = `[data-test="recipePeopleInput-${quantity}"]`;
+
+    await this.page.waitForSelector(selector);
+
+    await this.page.locator(selector).check();
+
+    await this.page.click(this.buttonSelector);
+
+    return await this.page.evaluate((prefix) => document.querySelectorAll(`[data-test^="${prefix}"]`).length, 'recipe-');
+  }
+
+  async filterByTime(min: number, max: number) {
+    const minSliderSelector = '[data-test="recipeMinTimeInput"]';
+    const maxSliderSelector = '[data-test="recipeMaxTimeInput"]';
+
+    await this.page.waitForSelector(minSliderSelector);
+    await this.page.waitForSelector(maxSliderSelector);
+
+    const minSlider = await this.page.$(minSliderSelector);
+    const maxSlider = await this.page.$(maxSliderSelector);
+
+    await minSlider?.evaluate((slider, min) => {
+      (slider as HTMLInputElement).value = String(min);
+      slider.dispatchEvent(new Event('input'));
+    }, min);
+
+    await maxSlider?.evaluate((slider, max) => {
+      (slider as HTMLInputElement).value = String(max);
+      slider.dispatchEvent(new Event('input'));
+    }, max);
+
+    await this.page.click(this.buttonSelector);
+
+    return await this.page.evaluate((prefix) => document.querySelectorAll(`[data-test^="${prefix}"]`).length, 'recipe-');
+  }
+
+  async filterByType(value: string) {
+    const selector = '[data-test="recipeTypeSelect"]';
+
+    await this.page.selectOption(selector, value);
+
+    await this.page.click(this.buttonSelector);
+
+    return await this.page.evaluate((prefix) => document.querySelectorAll(`[data-test^="${prefix}"]`).length, 'recipe-');
   }
 }
