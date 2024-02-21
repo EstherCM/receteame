fc3fe2dbbbdb260bd755d5b6726a80bc489f843b
EstherCM <esther.carreton8@gmail.com>
2024-02-21 17:32:40 +0100

    feat: refactor

---
+++ frontend/src/app/1_presentation/components/filters/filters.component.ts
@@ -0,0 +1,173 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, FormArray } from '@angular/forms';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatChipsModule, MatChipEditedEvent, MatChipInputEvent } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+
+import { RecipeRepository } from '../../../2_domain/repositories/recipe.class';
+import { IRecipe } from '../../../../../../backend/src/database/models/recipeModel';
+import { TypeRecipe } from '../../../2_domain/models/type-recipe.enum';
+
+@Component({
+  selector: 'app-filters',
+  standalone: true,
+  imports: [ReactiveFormsModule, MatSliderModule, MatFormFieldModule, MatChipsModule, MatIconModule],
+  animations: [],
+  templateUrl: './filters.component.html',
+  styleUrls: [
+    '../../../../styles/recipes-filters.scss',
+    '../../../../styles/components/checkbox.scss',
+    '../../../../styles/components/button.scss',
+  ],
+})
+export class FiltersComponent {
+  recipes: IRecipe[] = [];
+  searchRecipeForm = new FormGroup({
+    name: new FormControl(''),
+    ingredients: new FormArray([]),
+    people: new FormArray([]),
+    time: new FormGroup({
+      start: new FormControl(1),
+      end: new FormControl(240),
+    }),
+    type: new FormControl(),
+  });
+  ingredients: string[] = [];
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  people = [1, 2, 4, 8];
+  min = 0;
+  max = 240;
+  stepValue = 5;
+  types = [
+    TypeRecipe.starter,
+    TypeRecipe.first,
+    TypeRecipe.second,
+    TypeRecipe.dessert,
+    TypeRecipe.snack,
+    TypeRecipe.dessert,
+  ];
+
+  constructor(private recipeRepository: RecipeRepository) {}
+
+  addIngredient($event: MatChipInputEvent) {
+    const value = ($event.value || '').trim();
+    const ingredientForm = this.searchRecipeForm.get('ingredients') as FormArray;
+
+    if (value) {
+      this.ingredients.push(value);
+      ingredientForm.push(new FormControl(value));
+    }
+
+    $event.chipInput!.clear();
+  }
+
+  removeIngredient(ingredient: string) {
+    const index = this.ingredients.indexOf(ingredient);
+    const ingredientForm = this.searchRecipeForm.get('ingredients') as FormArray;
+
+    if (index >= 0) {
+      this.ingredients.splice(index, 1);
+      ingredientForm.removeAt(index);
+    }
+
+  }
+
+  editIngredient(ingredient: string, $event: MatChipEditedEvent) {
+    const value = $event.value.trim();
+
+    if (!value) {
+      this.removeIngredient(ingredient);
+      return;
+    }
+
+    const index = this.ingredients.indexOf(ingredient);
+    const ingredientForm = this.searchRecipeForm.get('ingredients') as FormArray;
+
+    if (index >= 0) {
+      const ingredientControl = ingredientForm.at(index) as FormControl;
+
+      this.ingredients[index] = value;
+      ingredientControl.setValue(value);
+    }
+  }
+
+  onChangePeople($event: Event) {
+    const checkedValue = ($event.target as HTMLInputElement).value;
+    const isChecked = ($event.target as HTMLInputElement).checked;
+    const checkedPeople = this.searchRecipeForm.get('people') as FormArray;
+
+    if (isChecked) {
+      checkedPeople.push(new FormControl(checkedValue));
+    } else {
+      let i = 0;
+
+      checkedPeople.controls.forEach((control) => {
+        if (control.value === checkedValue) {
+          checkedPeople.removeAt(i);
+        }
+        i++;
+      });
+    }
+  }
+
+  onSliderMinInput($event: Event) {
+    const sliderValue = ($event.target as HTMLInputElement).value;
+    const timeControl = this.searchRecipeForm.get('time');
+
+    if (timeControl) {
+      const { end } = timeControl.value;
+
+      timeControl.patchValue({
+        start: Number(sliderValue),
+        end,
+      });
+    }
+  }
+
+  onSliderMaxInput($event: Event) {
+    const sliderValue = ($event.target as HTMLInputElement).value;
+    const timeControl = this.searchRecipeForm.get('time');
+
+    if (timeControl) {
+      const { start } = timeControl.value;
+
+      timeControl.patchValue({
+        start,
+        end: Number(sliderValue),
+      });
+    }
+  }
+
+  formatLabel(value: number): string {
+    return String(value);
+  }
+
+  onChangeType($event: Event) {
+    const selectedValue = ($event.target as HTMLSelectElement).value;
+    const selectedType = this.searchRecipeForm.get('type') as FormControl;
+
+    selectedType.setValue(selectedValue);
+  }
+
+  onSubmit() {
+    const formValue = this.searchRecipeForm.value;
+
+    const filters = {
+      name: formValue.name || '',
+      ingredients: formValue.ingredients || [],
+      people: formValue.people || [],
+      time: {
+        start: formValue.time?.start || 1,
+        end: formValue.time?.end || 240,
+      },
+      type: formValue.type || undefined,
+    };
+
+    this.recipeRepository.get(filters).subscribe({
+      next: (recipes: IRecipe[]) => (this.recipes = recipes),
+      error: (error) => console.error('🔥 Error getting recipes:', error),
+    });
+  }
+}
--- frontend/src/app/1_presentation/components/recipes/recipes.component.ts
+++ frontend/src/app/1_presentation/components/recipes/recipes.component.ts
@@ -1,14 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, FormArray } from '@angular/forms';
-import {COMMA, ENTER} from '@angular/cdk/keycodes';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatChipsModule, MatChipEditedEvent, MatChipInputEvent } from '@angular/material/chips';
-import { MatIconModule } from '@angular/material/icon';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 import { IRecipe } from '../../../../../../backend/src/database/models/recipeModel';
 import { RecipeRepository } from '../../../2_domain/repositories/recipe.class';
-import { TypeRecipe } from '../../../2_domain/models/type-recipe.enum';
+import { FiltersComponent } from '../filters/filters.component';
 
 @Component({
   selector: 'app-recipes',
@@ -17,169 +11,39 @@ import { TypeRecipe } from '../../../2_domain/models/type-recipe.enum';
   styleUrls: [
     '../../../../styles/recipes-view.scss',
     '../../../../styles/recipe-list.scss',
-    '../../../../styles/recipe-item.scss',
-    '../../../../styles/recipes-filters.scss',
-    '../../../../styles/checkbox.scss',
-    '../../../../styles/button.scss',
+    '../../../../styles/recipe-item.scss'
   ],
-  imports: [ReactiveFormsModule, MatSliderModule, MatFormFieldModule, MatChipsModule, MatIconModule],
-  animations: []
+  imports: [FiltersComponent]
 })
 export class RecipesComponent implements OnInit {
   recipes: IRecipe[] = [];
+  showFilterBar = false;
 
-  searchRecipeForm = new FormGroup({
-    name: new FormControl(''),
-    ingredients: new FormArray([]),
-    people: new FormArray([]),
-    time: new FormGroup({
-      start: new FormControl(1),
-      end: new FormControl(240),
-    }),
-    type: new FormControl(),
-  });
-
-  ingredients: string[] = [];
-  separatorKeysCodes: number[] = [ENTER, COMMA];
-  people = [1, 2, 4, 8];
-  min = 0;
-  max = 240;
-  stepValue = 5;
-  types = [
-    TypeRecipe.starter,
-    TypeRecipe.first,
-    TypeRecipe.second,
-    TypeRecipe.dessert,
-    TypeRecipe.snack,
-    TypeRecipe.dessert,
-  ];
-
-  constructor(private recipeRepository: RecipeRepository) {}
+  constructor(private recipeRepository: RecipeRepository, private el: ElementRef) {}
 
   ngOnInit(): void {
     this.recipeRepository.get({}).subscribe({
       next: (recipes: IRecipe[]) => (this.recipes = recipes),
       error: (error) => console.error('🔥 Error getting recipes:', error),
     });
+    document.addEventListener('click', (event) => this.handleOutFilterClick(event));
   }
 
-  addIngredient($event: MatChipInputEvent) {
-    const value = ($event.value || '').trim();
-    const ingredientForm = this.searchRecipeForm.get('ingredients') as FormArray;
+  closeFilters() {
+    this.showFilterBar = false;
+  }
 
-    if (value) {
-      this.ingredients.push(value);
-      ingredientForm.push(new FormControl(value));
+  toggleFilterBar(event: Event): void {
+    event.stopPropagation();
+
+    this.showFilterBar = !this.showFilterBar;
+  }
+
+  handleOutFilterClick(event: Event) {
+    const appFiltersElement = this.el.nativeElement.querySelector('app-filters');
+
+    if (this.showFilterBar && event.target !== appFiltersElement) {
+      this.closeFilters();
     }
-
-    $event.chipInput!.clear();
-  }
-
-  removeIngredient(ingredient: string) {
-    const index = this.ingredients.indexOf(ingredient);
-    const ingredientForm = this.searchRecipeForm.get('ingredients') as FormArray;
-
-    if (index >= 0) {
-      this.ingredients.splice(index, 1);
-      ingredientForm.removeAt(index);
-    }
-
-  }
-
-  editIngredient(ingredient: string, $event: MatChipEditedEvent) {
-    const value = $event.value.trim();
-
-    if (!value) {
-      this.removeIngredient(ingredient);
-      return;
-    }
-
-    const index = this.ingredients.indexOf(ingredient);
-    const ingredientForm = this.searchRecipeForm.get('ingredients') as FormArray;
-
-    if (index >= 0) {
-      const ingredientControl = ingredientForm.at(index) as FormControl;
-
-      this.ingredients[index] = value;
-      ingredientControl.setValue(value);
-    }
-  }
-
-  onChangePeople($event: Event) {
-    const checkedValue = ($event.target as HTMLInputElement).value;
-    const isChecked = ($event.target as HTMLInputElement).checked;
-    const checkedPeople = this.searchRecipeForm.get('people') as FormArray;
-
-    if (isChecked) {
-      checkedPeople.push(new FormControl(checkedValue));
-    } else {
-      let i = 0;
-
-      checkedPeople.controls.forEach((control) => {
-        if (control.value === checkedValue) {
-          checkedPeople.removeAt(i);
-        }
-        i++;
-      });
-    }
-  }
-
-  onSliderMinInput($event: Event) {
-    const sliderValue = ($event.target as HTMLInputElement).value;
-    const timeControl = this.searchRecipeForm.get('time');
-
-    if (timeControl) {
-      const { end } = timeControl.value;
-
-      timeControl.patchValue({
-        start: Number(sliderValue),
-        end,
-      });
-    }
-  }
-
-  onSliderMaxInput($event: Event) {
-    const sliderValue = ($event.target as HTMLInputElement).value;
-    const timeControl = this.searchRecipeForm.get('time');
-
-    if (timeControl) {
-      const { start } = timeControl.value;
-
-      timeControl.patchValue({
-        start,
-        end: Number(sliderValue),
-      });
-    }
-  }
-
-  formatLabel(value: number): string {
-    return String(value);
-  }
-
-  onChangeType($event: Event) {
-    const selectedValue = ($event.target as HTMLSelectElement).value;
-    const selectedType = this.searchRecipeForm.get('type') as FormControl;
-
-    selectedType.setValue(selectedValue);
-  }
-
-  onSubmit() {
-    const formValue = this.searchRecipeForm.value;
-
-    const filters = {
-      name: formValue.name || '',
-      ingredients: formValue.ingredients || [],
-      people: formValue.people || [],
-      time: {
-        start: formValue.time?.start || 1,
-        end: formValue.time?.end || 240,
-      },
-      type: formValue.type || undefined,
-    };
-
-    this.recipeRepository.get(filters).subscribe({
-      next: (recipes: IRecipe[]) => (this.recipes = recipes),
-      error: (error) => console.error('🔥 Error getting recipes:', error),
-    });
   }
 }
